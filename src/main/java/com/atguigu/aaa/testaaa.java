23b272b5fe2bd14394da3e251cdea01692bbc137
15702428315 <330230211@qq.com>
2020-09-07 21:06:41 +0800

    最终测试

---
+++ src/main/java/com/atguigu/aaa/testaaa.java
@@ -0,0 +1,11 @@
+package com.atguigu.aaa;
+
+/**
+ * @author shkstart
+ * @create 2020-09-07 20:04
+ */
+public class testaaa {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
